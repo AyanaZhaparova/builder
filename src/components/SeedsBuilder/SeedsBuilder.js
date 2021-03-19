35001ccf20bbf87a1f48b07bd0a18445c41d8809
Ayana Zhaparova <ayana.zhapar@gmail.com>
2021-03-19 16:27:18 +0600

    rgethteTHeth er4g

---
+++ src/components/SeedsBuilder/SeedsBuilder.js
@@ -0,0 +1,11 @@
+import classes from "*.module.css"
+
+const SeedsBuilder = () => {
+    return (
+        <div className={classes.SeedsBuilder}>
+            <SeedsPreview />
+            <SeedsControls />
+
+        </div>
+    )
+}
