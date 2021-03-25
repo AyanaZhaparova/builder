50f0eb408188c8166c7b5666950ccb23d549894f
Ayana Zhaparova <ayana.zhapar@gmail.com>
2021-03-25 14:04:16 +0600

    Changed App.js

--- src/App.js
+++ src/App.js
@@ -2,11 +2,11 @@ import Layout from "./components/Layout/Layout";
 import SeedsBuilder from "./components/SeedsBuilder/SeedsBuilder";
 import "./App.css";
 
-const App() {
+const App = () => {
   return (
     <div className="App">
       <Layout>
-        <SeedsBuilder />
+        <PizzaBuilder />
       </Layout>
     </div>
   );
