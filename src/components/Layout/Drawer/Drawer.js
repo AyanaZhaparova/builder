d16adc93d8a8de9b0e02b31a8c9b7ceef8dd5a56
Ayana Zhaparova <ayana.zhapar@gmail.com>
2021-04-07 14:52:01 +0600

    xhgjnbcvgfgh

--- src/components/Layout/Drawer/Drawer.js
+++ src/components/Layout/Drawer/Drawer.js
@@ -1,6 +1,6 @@
 import Logo from "../../UI/Logo/Logo";
 import Nav from "../../Nav/Nav";
-import Backdrop from "../../UI/Backdrop";
+import Backdrop from "../../UI/Backdrop/Backdrop";
 import classes from "./Drawer.module.css";
 
 const Drawer = ({ open, closeDrawer }) => {
--- src/components/SeedsBuilder/SeedsBuilder.js
+++ src/components/SeedsBuilder/SeedsBuilder.js
@@ -1,18 +1,88 @@
-import classes from "./SeedsBuilder.module.css";
+// import classes from "./SeedsBuilder.module.css";
+// import SeedsPreview from "./SeedsPreview/SeedsPreview";
+// import SeedsControls from "./SeedsControls/SeedsControls";
+
+// const SeedsBuilder = () => {
+//   const flowers = {
+//     rose: 5,
+//     orchids: 5,
+//     ochamomile: 5,
+//   };
+
+//   return (
+//     <div className={classes.SeedsBuilder}>
+//       <SeedsPreview flowers={flowers} />
+//       <SeedsControls />
+//     </div>
+//   );
+// }
+
+// export default SeedsBuilder;
+
 import SeedsPreview from "./SeedsPreview/SeedsPreview";
 import SeedsControls from "./SeedsControls/SeedsControls";
 
+import classes from "./SeedsBuilder.module.css";
+import { useState } from "react";
+import OrderSummary from "./OrderSummary/OrderSummary";
+import Modal from "../UI/Modal/Modal";
+
 const SeedsBuilder = () => {
-  const flowers = {
+  const prices = {
     rose: 5,
     orchids: 5,
     ochamomile: 5,
   };
 
+  const [flowers, setFlowers] = useState({
+    rose: 5,
+    orchids: 5,
+    ochamomile: 5,
+  });
+  const [price, setPrice] = useState(150);
+  const [canBuy, setCanBuy] = useState(true);
+  const [isBuying, setIsBuying] = useState(false);
+
+  function checkCanBuy(newFlowers) {
+    const totalFlowers = Object.values(newFlowers)
+      .reduce((total, current) => total + current);
+    setCanBuy(totalFlowers > 0);
+  }
+
+  function addFlower(type) {
+    const newFlowers = { ...flowers };
+    newFlowers[type]++;
+    checkCanBuy(newFlowers);
+    setPrice(price + prices[type]);
+    setFlowers(newFlowers);
+  }
+
+  function removeFlower(type) {
+    if (flowers[type]) {
+      const newFlowers = { ...flowers };
+      newFlowers[type]--;
+      checkCanBuy(newFlowers);
+      setPrice(price - prices[type]);
+      setFlowers(newFlowers);
+    }
+  }
+
   return (
     <div className={classes.SeedsBuilder}>
-      <SeedsPreview flowers={flowers} />
-      <SeedsControls />
+      <Modal show={isBuying} cancelCallback={() => setIsBuying(false)}>
+        <OrderSummary flowers={flowers} price={price} />
+      </Modal>
+
+      <SeedsPreview
+        flowers={flowers}
+        price={price} />
+      <SeedsControls
+        canBuy={canBuy}
+        setIsBuying={setIsBuying}
+        flowers={flowers}
+        addFlower={addFlower}
+        removeFlower={removeFlower}
+        />
     </div>
   );
 }
