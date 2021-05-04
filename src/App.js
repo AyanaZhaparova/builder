cc7fa7376c27146da2bb5bc2a7bb201dc02fcd18
Ayana Zhaparova <ayana.zhapar@gmail.com>
2021-05-04 14:06:30 +0600

    install react-redux packages

--- src/App.js
+++ src/App.js
@@ -1,12 +1,19 @@
 import Layout from "./components/Layout/Layout";
 import SeedsBuilder from "./components/SeedsBuilder/SeedsBuilder";
+import Checkout from "./Checkout/Checkout";
 import "./App.css";
+import { Redirect, Route, Switch } from "react-router";
 
 const App = () => {
   return (
     <div className="App">
       <Layout>
-        <SeedsBuilder />
+        <Switch>
+          <Route path="/" component={SeedsBuilder} exact />
+          <Route path="/checkout" component={Checkout} />
+          <Redirect to="/" />;
+        </Switch>
+        
       </Layout>
     </div>
   );
