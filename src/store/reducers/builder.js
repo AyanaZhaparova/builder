2244c78f5a9102b040c2a176b556bb2665730200
Ayana Zhaparova <ayana.zhapar@gmail.com>
2021-05-11 23:11:55 +0600

    added builder.js

---
+++ src/store/reducers/builder.js
@@ -0,0 +1,42 @@
+const initialState = {
+    flowers: {
+        rose: 10,
+        plumeria: 10,
+        lily: 10,
+        gerbera: 10,
+        chamomile: 10,
+        orchids: 10,
+    },
+    price: 200,
+};
+
+const prices = {
+        rose: 5,
+        plumeria: 5,
+        lily: 5,
+        gerbera: 5,
+        chamomile: 5,
+        orchids: 5,
+};
+
+const builder = (state = initialState, action) => {
+      const newState = { ...state };
+
+      switch (action.type) {
+       case "ADD_VIEWS":
+          newState.flowers[action.flower]++;
+          newState.price += prices[action.flower];
+        break;
+        case "REMOVE_VIEWS":
+          newState.flowers[action.flower]--;
+          newState.price -= prices[action.flower];
+          break;
+
+          default:
+          break;
+      }
+
+    return newState;
+}
+
+export default builder;
